e4438fa28157df64028ce5d1bcb60a9b3a51028d
Ricardo Ramos <rf.ramos95@gmail.com>
2021-03-16 16:30:00 +0000

    Implement algorithm for random message generator

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Mixed messages script");
+const type = ['website', 'game', 'app', 'web scrapper'];
+const language = ['HTML/CSS', 'Javascript', 'Python', 'Java', 'C#'];
+const theme = ['madlib creation', 'a small business', 'a pet store', 'a social media'];
+
+const randomIndex = maxLength => Math.floor(Math.random() * maxLength);
+
+const message = () => {
+    let typeIndex = randomIndex(type.length);
+    let languageIndex = randomIndex(language.length);
+    let themeIndex = randomIndex(theme.length);
+
+    return `Develop a ${type[typeIndex]} using ${language[languageIndex]}, if possible, for ${theme[themeIndex]}.`;
+}
+
+console.log(message());
